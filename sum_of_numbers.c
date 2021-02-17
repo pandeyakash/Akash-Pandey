c41270f2121eca51593467b45caaafdb0ca5af81
pandeyakash <61912643+pandeyakash@users.noreply.github.com>
2021-02-17 10:20:36 +0530

    Create sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,37 @@
 //Write a program to find the sum of n different numbers using 4 functions
+
+#include <stdio.h>
+ 
+#include<stdio.h>
+int input()
+{
+    int n;
+    printf("Enter the number of integers=");
+    scanf("%d",&n);
+    return n;
+}
+int calc()
+{
+    int a[100],i,sum=0,n;
+    n=input();
+    printf("Enter the numbers=");
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    return(sum);
+}
+int print()
+{
+    int n,sum;
+    
+    sum=calc();
+    printf("The sum of numbers is %d",sum);
+}
+int main()
+{
+    print();
+    return 0;
+}
